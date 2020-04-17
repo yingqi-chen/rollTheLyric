e5ec1599f01371e2099d79c7fac74c9cc30c73db
chanwkkk <chanwk18@hotmail.com>
2020-04-17 15:36:08 -0600

    first draft

---
+++ rolling.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+const file = fs.readFile("./lyric.txt", (err,data)=>{
+    if(err) {
+        console.log(err)
+     } else{
+         printLyric(data.toString())
+     }
+})
+
+function printLyric(lyric){
+  const reg = /\[(\d{2})\:(\d{2})\.(\d{2})\]\s*(.+)/
+  const lines = lyric.split("\n")
+  for(let i = 0; i<lines.length; i++){
+    let line = lines[i];
+    let matches = reg.exec(line)
+    if(matches){
+      let minute = parseFloat(matches[1])
+      let second = parseFloat(matches[2])
+      let millsecond = parseFloat(matches[3])
+      let content = matches[4]
+      let time = minute*60*1000 + second*1000 + millsecond
+      setTimeout(function(){
+        console.log(content)
+      },time)
+    }
+  }
+
+}
